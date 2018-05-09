7c7a27822bebffa71e4d95113167d96a61203525
Eugenie <eugenie-heijn@hotmail.com>
2018-05-09 21:14:56 +0200

    begonnen aan opdracht 3

---
+++ Opdrachten weeek 2/KaartSpel/KaartSpel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_3
+{
+    public class KaartSpel
+    {
+        //fields
+        public int aantalSpelers;
+        List<SpeelKaart> AlleSpeelKaarten = new List<SpeelKaart>()
+             
+
+    }
+
+    public class SpeelKaart : KaartSpel
+    {
+        //fields
+        public int nummer;
+    }
+
+    public enum KaartType
+    {
+        Schoppen, Klaveren, Harten, Ruiten
+    }
+}
